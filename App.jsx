6ec76d9a2ffa163ae6d6ca6bf531f5f7f4b195a8
Onkar <onkaryadav096@gmail.com>
2024-10-30 10:18:33 +0530

    First commit for react native code

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import onPress from './src/components/onPress';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+    <Text>Hello Onkar</Text>
+    <onPress />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/components/OnPress.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const onPress = () => {
+  return (
+    <View>
+      <Text>Test onPress</Text>
+    </View>
+  )
+}
+
+export default onPress
